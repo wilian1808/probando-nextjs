31b49b53a1adbe4e8d27a6a9646491abfc4a8b99
wilian1808 <wilian.walli@gmail.com>
2022-12-11 14:25:33 -0500

    change

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const text = process.env.greet
+  const text = process.env.NEXT_PUBLIC_GITHUB
   console.log(text);
 
   return (
