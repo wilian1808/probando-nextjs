f73ae5e3821c2322f7a7c0c26e1260adaf569e2b
wilian1808 <wilian.walli@gmail.com>
2022-12-10 23:29:58 -0500

    change title

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <a href="https://nextjs.org">{ text }</a>
+          <a href="https://nextjs.org">{ text } - .env</a>
         </h1>
 
         <p className={styles.description}>
