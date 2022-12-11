160ba76b02f0c1ca80b3eccf5f689c0788ead4b2
wilian1808 <wilian.walli@gmail.com>
2022-12-11 00:04:52 -0500

    add new page

---
+++ pages/hello.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Hello () {
+  return (
+    <div>
+      <Link href='/'>home</Link>
+      <hr />
+      <h1>hello world...</h1>
+    </div>
+  )
+}
